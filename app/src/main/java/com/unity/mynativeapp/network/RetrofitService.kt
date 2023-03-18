57c436134454e2e59ae98bfe84c2bad9646ea7dc
yerim425 <lyl40255@gmail.com>
2023-03-18 21:03:23 +0900

    login

---
+++ app/src/main/java/com/unity/mynativeapp/network/RetrofitService.kt
@@ -0,0 +1,21 @@
+package com.unity.mynativeapp.network
+
+import com.unity.mynativeapp.model.*
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface RetrofitService {
+
+	@POST("signin")
+	fun login(@Body loginRequest: LoginRequest): Call<MyResponse<LoginData>>
+
+	@POST("signup")
+	fun signup(@Body signUpRequest: SignUpRequest) : Call<MyResponse<String>>
+
+	// 홈 화면
+	@GET("/home")
+	fun getHomePage( @Query("userIdx") userIdx: Int) : Call<HomePageResponse>
+}
