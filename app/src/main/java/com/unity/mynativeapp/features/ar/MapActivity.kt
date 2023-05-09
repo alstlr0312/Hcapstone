58bcdb5a2a8952440b8e0de6cfe8b14f89eb3b84
alstlr0312 <90487082+alstlr0312@users.noreply.github.com>
2023-05-09 18:33:44 +0900

    Map연결

--- app/src/main/java/com/unity/mynativeapp/features/ar/MapActivity.kt
+++ app/src/main/java/com/unity/mynativeapp/features/ar/MapActivity.kt
@@ -2,26 +2,76 @@ package com.unity.mynativeapp.features.ar
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.annotation.NonNull
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 import com.unity.mynativeapp.R
 
 class MapActivity : Activity(), OnMapReadyCallback {
     private lateinit var mapView: MapView
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
     private lateinit var naverMap: NaverMap
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_walk_map_fragment)
-        // 네이버 지도
-        mapView = findViewById(R.id.map_view)
+        setContentView(R.layout.activity_map)
+
+        mapView = findViewById(R.id.Walkmap_view)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
+
     }
 
-    override fun onMapReady(naverMap: NaverMap) {
+
+    override fun onMapReady(@NonNull naverMap: NaverMap) {
         this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
