85ecdf441ee28617c832561aa6fe02cfd32edbc1
yerim425 <lyl40255@gmail.com>
2023-07-03 06:10:08 +0900

    find id pw

--- app/src/main/java/com/unity/mynativeapp/model/CheckResponse.kt
+++ app/src/main/java/com/unity/mynativeapp/model/CheckResponse.kt
@@ -2,7 +2,7 @@ package com.unity.mynativeapp.model
 
 import com.google.gson.annotations.SerializedName
 
-data class CheckData(
+data class CheckResponse(
     @SerializedName("target") val target: String,
     @SerializedName("code") val code: String
 )
